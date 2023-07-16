0fce381de087ec6924432989895ea527d0a1f9ee
Piotr Łojko <80154448+S19514@users.noreply.github.com>
2023-07-16 16:27:41 +0200

    fix grid

--- src/js/index.js
+++ src/js/index.js
@@ -91,18 +91,24 @@ function createImage(image) {
     downloads,
   } = image;
 
-  return `
-    <div class="photo-card">
-    <a href="${largeImageURL}" data-lightbox="gallery" data-title="${tags}">
-      <img src="${webformatURL}" alt="${tags}" loading="lazy">
-      <div class="info">
-        <p class="info-item"><b>Likes:</b> ${likes}</p>
-        <p class="info-item"><b>Views:</b> ${views}</p>
-        <p class="info-item"><b>Comments:</b> ${comments}</p>
-        <p class="info-item"><b>Downloads:</b> ${downloads}</p>
-      </div>
+  return `<div class="photo-card">
+        <a href="${largeImageURL}" data-lightbox="gallery" data-title="${tags}">
+    <div class="thumb"><img src="${webformatURL}" alt="${tags}" loading="lazy" /></div>
+    <div class="info">
+      <p class="info-item">
+        <b>Likes</b><span>${likes}</span>
+      </p>
+      <p class="info-item">
+        <b>Views</b><span>${views}</span>
+      </p>
+      <p class="info-item">
+        <b>Comments</b><span>${comments}</span>
+      </p>
+      <p class="info-item">
+        <b>Downloads</b><span>${downloads}</span>
+      </p>
     </div>
-  `;
+  </div>`;
 }
 
 loadMoreBtn.addEventListener('click', loadMoreImages);
